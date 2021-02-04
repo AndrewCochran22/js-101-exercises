dbee9457f5946f75f373fe0126eec55ffa9ad5ae
Andrew Cochran <acoch22@gmail.com>
2021-02-04 17:02:56 -0500

    added javascript exercises

--- exercises/level3.js
+++ exercises/level3.js
@@ -1,24 +1,28 @@
 function lvl3exercise1 () {
   // Create a "hello" and a "world", return the concatenation of the two
-
+hell = "hello"
+worl = "world"
+return hell + worl
 }
 
 function lvl3exercise2 () {
   // Create a "hello" and a 12, return the concatenation of the two
-
+return l3e2 = "hello" + 12
 }
 
 function lvl3exercise3 () {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-
+const variable = 12
+return l3e3 = 12 + ""
 }
 
 function lvl3exercise4 () {
   // Create a "hello world!" string. Return the length of the string
-
+return l3e4 = ("hello world!").length
 }
 
 function lvl3exercise5 () {
   // Create a "hello world!" string. Return the index of the word "world".
-
+const word = "hello world!"
+return word.indexOf("world")
 }
